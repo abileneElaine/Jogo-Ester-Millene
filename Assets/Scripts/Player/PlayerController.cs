cf97e31b54f00fb6fb9c8f6205499baf4db1a1a6
abileneElaine <145374967+abileneElaine@users.noreply.github.com>
2024-12-12 08:20:00 -0300

    aula2(12/12

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,15 +7,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private float moveX;
     public float speed;
+    public int addJumps;
+    public bool isGrounded;
+    public float jumpForce;
 
 
-// Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         moveX = Input.GetAxisRaw("Horizontal");
@@ -24,9 +25,58 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Move();
-    }
-    void Move()
-    {
-        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
-    }
-}
+    
+
+     if(isGrounded == true)
+     {
+         addJumps = 1;
+         if(Input.GetButtonDown("Jump"))
+         {
+             Jump();
+         }
+         else
+         {
+             if(Input.GetButtonDown("Jump") && addJumps > 0)
+             {
+                 addJumps--;
+                 Jump();
+             }
+         }
+     }
+
+     void Move()
+     {
+            rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+
+            if(moveX >  0)
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+
+            }
+            if(moveX < 0)
+            {
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            }
+     }
+     void Jump()
+     {
+         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+     }
+
+     void OnCollisionEnter2D(Collision2D collision)
+     {
+         if(collision.gameObject.tag == "Ground")
+         {
+             isGrounded = true;
+         }
+     }
+
+     void OnCollisionExit2D(Collision2D collision)
+     {
+         if(collision.gameObject.tag == "Ground")
+         {
+             isGrounded = false;
+         }
+     }
+   
+}   }
